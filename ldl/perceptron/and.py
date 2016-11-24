03b737a9cd1f11c9287dfd45dd66307003de7d2e
Hideyoshi Moriya <hide@kin2ku.org>
2016-11-25 06:55:53 +0800

    ANDゲートを実装したよ！ #4

---
+++ ldl/perceptron/and.py
@@ -0,0 +1,12 @@
+def AND(x1, x2):
+	w1, w2, theta = 1.0, 1.0, 1.0
+	tmp = x1*w1 + x2*w2
+	if tmp <= theta:
+		return 0
+	else:
+		return 1
+
+print(AND(0,0))
+print(AND(0,1))
+print(AND(1,0))
+print(AND(1,1))
