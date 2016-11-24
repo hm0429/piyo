b714cb5e71d93f526832fd836973e41a052aa9b1
Hideyoshi Moriya <hide@kin2ku.org>
2016-11-25 07:16:56 +0800

    NANDゲートを実装したよ！ #6

---
+++ ldl/perceptron/nand.py
@@ -0,0 +1,12 @@
+def NAND(x1, x2):
+	w1, w2, theta = -0.5, -0.5, -1.0
+	tmp = x1*w1 + x2*w2
+	if tmp <= theta:
+		return 0
+	else:
+		return 1
+
+print(NAND(0,0))
+print(NAND(0,1))
+print(NAND(1,0))
+print(NAND(1,1))
