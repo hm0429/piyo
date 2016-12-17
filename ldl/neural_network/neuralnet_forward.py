177f10171a5d4ddaa01bb6e44d0e1699394d8e2b
Hideyoshi Moriya <hide@kin2ku.org>
2016-12-17 16:40:27 +0800

    #17
    本を読んでそのまま書いたものをひとまずこみっと

---
+++ ldl/neural_network/neuralnet_forward.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pylab as mpl
+
+def sigmoid(x):
+	return 1 / (1+ np.exp(-x))
+
+def identify_function(x):
+	return x
+
+X = np.array([1.0, 0.5])		## 入力信号
+W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])		## 1層目の重み
+B1 = np.array([0.1, 0.2, 0.3])		## バイアス
+
+print(W1.shape)
+print(X.shape)
+print(B1.shape)
+
+A1 = np.dot(X, W1) + B1
+
+print(A1)
+
+Z1 = sigmoid(A1)
+
+print(Z1)
+
+W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+B2 = np.array([0.1, 0.2])
+
+print(Z1.shape)
+print(W2.shape)
+print(B2.shape)
+
+A2 = np.dot(Z1, W2) + B2
+Z2 = sigmoid(A2)
+
+print(Z2)
+
+W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
+B3 = np.array([0.1, 0.2])
+
+A3 = np.dot(Z2, W3) + B3
+Y = identify_function(A3)
+
+print(Y)
