7205a3c25b47749fcb2ad1b978748970c6b119ec
Hideyoshi Moriya <hide@kin2ku.org>
2016-12-18 19:37:30 +0800

    #17

--- ldl/neural_network/neuralnet_forward.py
+++ ldl/neural_network/neuralnet_forward.py
@@ -7,38 +7,36 @@ def sigmoid(x):
 def identify_function(x):
 	return x
 
-X = np.array([1.0, 0.5])		## 入力信号
-W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])		## 1層目の重み
-B1 = np.array([0.1, 0.2, 0.3])		## バイアス
+def init_network():
+	network = {}
 
-print(W1.shape)
-print(X.shape)
-print(B1.shape)
+	network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])		## 第1層目の重み
+	network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])		## 第2層目の重み
+	network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])					## 第3層目の重み
+	
+	network['b1'] = np.array([0.1, 0.2, 0.3])							## 第1層目のバイアス
+	network['b2'] = np.array([0.1, 0.2])								## 第2層目のバイアス
+	network['b3'] = np.array([0.1, 0.2])								## 第3層目のバイアス
+	
+	return network
 
-A1 = np.dot(X, W1) + B1
 
-print(A1)
+def forward(network, x):
+	W1, W2, W3 = network['W1'], network['W2'], network['W3']
+	b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-Z1 = sigmoid(A1)
+	a1 = np.dot(x, W1) + b1
+	z1 = sigmoid(a1)
 
-print(Z1)
+	a2 = np.dot(z1, W2) + b2
+	z2 = sigmoid(a2)
 
-W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-B2 = np.array([0.1, 0.2])
+	a3 = np.dot(z2, W3) + b3
+	y = identify_function(a3)
 
-print(Z1.shape)
-print(W2.shape)
-print(B2.shape)
+	return y
 
-A2 = np.dot(Z1, W2) + B2
-Z2 = sigmoid(A2)
-
-print(Z2)
-
-W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
-B3 = np.array([0.1, 0.2])
-
-A3 = np.dot(Z2, W3) + B3
-Y = identify_function(A3)
-
-print(Y)
+network = init_network()
+x = np.array([1.0, 0.5])
+y = forward(network, x)
+print(y)
